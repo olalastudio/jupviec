6f8f431b59a62635ee10a1b8176a41391f2f603b
Olala Studio <olalastudios@gmail.com>
2020-01-07 18:12:37 +0700

    draw confirm order view follow GUI
    draw confirm order view follow GUI

--- View/HomeView/ConfirmOrder/ConfirmOrderViewController.h
+++ View/HomeView/ConfirmOrder/ConfirmOrderViewController.h
@@ -21,8 +21,24 @@ NS_ASSUME_NONNULL_BEGIN
 @property Order     *order;
 @property User      *user;
 
-@property (weak, nonatomic) IBOutlet UITextView *txtContent;
-@property (weak, nonatomic) IBOutlet UILabel *txtTotalMoney;
+@property (weak, nonatomic) IBOutlet UILabel *txtType;
+@property (weak, nonatomic) IBOutlet UILabel *txtAddressTitle;
+@property (weak, nonatomic) IBOutlet UILabel *txtAddressValue;
+@property (weak, nonatomic) IBOutlet UILabel *txtPhoneNumberTitle;
+@property (weak, nonatomic) IBOutlet UILabel *txtPhoneNumberValue;
+@property (weak, nonatomic) IBOutlet UILabel *txtWorkDateTitle;
+@property (weak, nonatomic) IBOutlet UILabel *txtWorkDateValue;
+@property (weak, nonatomic) IBOutlet UILabel *txtWorkTimeTitle;
+@property (weak, nonatomic) IBOutlet UILabel *txtWorkTimeValue;
+@property (weak, nonatomic) IBOutlet UILabel *txtWorkHourTitle;
+@property (weak, nonatomic) IBOutlet UILabel *txtWorkHourValue;
+@property (weak, nonatomic) IBOutlet UILabel *txtExtendServiceTitle;
+@property (weak, nonatomic) IBOutlet UILabel *txtExtendServiceValue;
+@property (weak, nonatomic) IBOutlet UILabel *txtPaymentMethodTitle;
+@property (weak, nonatomic) IBOutlet UILabel *txtPaymentMethodValue;
+@property (weak, nonatomic) IBOutlet UILabel *txtNoteTitle;
+@property (weak, nonatomic) IBOutlet UILabel *txtNoteValue;
+@property (weak, nonatomic) IBOutlet UILabel *txtTotalMoneyTitle;
 @property (weak, nonatomic) IBOutlet UILabel *txtTotalMoneyValue;
 @property (weak, nonatomic) IBOutlet UIButton *btConfirm;
 
